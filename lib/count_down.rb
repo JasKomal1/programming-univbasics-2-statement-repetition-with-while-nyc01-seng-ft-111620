3ccc995026fe03ef94c616dcd9dc8c3b7cfad4b1
jsr <jaskomal2@gmail.com>
2020-10-25 00:28:53 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,11 +2,9 @@
 exit_num = 0 
 count = 10 
 
-while count > -1 do 
-  if count == 0 
-    puts 'Happy New Year!'
-  else 
-    puts count 
-  end
+while count >= 1 do 
+  puts count
   count -= 1 
 end 
+
+puts 'Happy New Year!'
